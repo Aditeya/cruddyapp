355c22a133775c88ebb9bef0dbb0e3b309815595
aditeya <22963960+Aditeya@users.noreply.github.com>
2022-04-08 15:57:28 +0400

    formatted post-creater component

--- src/app/post-creater/post-creater.component.spec.ts
+++ src/app/post-creater/post-creater.component.spec.ts
@@ -3,23 +3,22 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { PostCreaterComponent } from './post-creater.component';
 
 describe('PostCreaterComponent', () => {
-  let component: PostCreaterComponent;
-  let fixture: ComponentFixture<PostCreaterComponent>;
+	let component: PostCreaterComponent;
+	let fixture: ComponentFixture<PostCreaterComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ PostCreaterComponent ]
-    })
-    .compileComponents();
-  });
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			declarations: [PostCreaterComponent],
+		}).compileComponents();
+	});
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(PostCreaterComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(() => {
+		fixture = TestBed.createComponent(PostCreaterComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
--- src/app/post-creater/post-creater.component.ts
+++ src/app/post-creater/post-creater.component.ts
@@ -5,62 +5,60 @@ import { Post } from '../post';
 import { PostCreaterService } from './post-creater.service';
 
 @Component({
-  selector: 'app-post-creater',
-  templateUrl: './post-creater.component.html',
-  styleUrls: ['./post-creater.component.css']
+	selector: 'app-post-creater',
+	templateUrl: './post-creater.component.html',
+	styleUrls: ['./post-creater.component.css'],
 })
 export class PostCreaterComponent implements OnInit {
-  
-  returnPost: Observable<any> | undefined;
-  postForm: FormGroup;
-  radioButtonValues = [
-    'post',
-    'put',
-    'patch',
-    'delete'
-  ];
+	returnPost: Observable<any> | undefined;
+	postForm: FormGroup;
+	radioButtonValues = ['post', 'put', 'patch', 'delete'];
 
-  constructor(private fb: FormBuilder, private pcService: PostCreaterService) {
-    this.postForm = this.fb.group({
-      resttype: '',
-      id: '',
-      userId: '',
-      title: '',
-      body: ''
-    });
-  }
+	constructor(
+		private fb: FormBuilder,
+		private pcService: PostCreaterService
+	) {
+		this.postForm = this.fb.group({
+			resttype: '',
+			id: '',
+			userId: '',
+			title: '',
+			body: '',
+		});
+	}
 
-  ngOnInit(): void {
-    this.postForm.valueChanges.subscribe(console.log);
-  }
-  
-  log(x: any) {
-    console.log(x);
-  }
-  
-  onSubmit() {
-    let post: Post = {
-      id: (this.postForm.value.id == '') ? 0: this.postForm.value.id, 
-      userId: Number(this.postForm.value.userId),
-      title: this.postForm.value.title,
-      body: this.postForm.value.body
-    }
+	ngOnInit(): void {
+		this.postForm.valueChanges.subscribe(console.log);
+	}
 
-    switch(this.postForm.value.resttype) {
-      case "post":
-        this.returnPost = this.pcService.createPost(post);
-        break;
-      case "put":
-        this.returnPost = this.pcService.updatePostFull(post);
-        break;
-      case "patch":
-        this.returnPost = this.pcService.updatePostPartial(post);
-        break;
-      case "delete":
-        this.returnPost = this.pcService.deletePost(this.postForm.value.id);
-        break;
-      default:
-    }
-  }
+	log(x: any) {
+		console.log(x);
+	}
 
+	onSubmit() {
+		const post: Post = {
+			id: this.postForm.value.id == '' ? 0 : this.postForm.value.id,
+			userId: Number(this.postForm.value.userId),
+			title: this.postForm.value.title,
+			body: this.postForm.value.body,
+		};
+
+		switch (this.postForm.value.resttype) {
+			case 'post':
+				this.returnPost = this.pcService.createPost(post);
+				break;
+			case 'put':
+				this.returnPost = this.pcService.updatePostFull(post);
+				break;
+			case 'patch':
+				this.returnPost = this.pcService.updatePostPartial(post);
+				break;
+			case 'delete':
+				this.returnPost = this.pcService.deletePost(
+					this.postForm.value.id
+				);
+				break;
+			default:
+		}
+	}
 }
