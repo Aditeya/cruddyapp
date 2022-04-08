8b03e81b681f4ac20fca5d069cd34f75c0f86c2a
aditeya <22963960+Aditeya@users.noreply.github.com>
2022-04-08 18:56:43 +0400

    formatted app component and module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,24 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PostsComponent } from './posts/posts.component';
-import { PostsService } from './posts/posts.service';
 import { PostCreaterComponent } from './post-creater/post-creater.component';
 import { PostCreaterService } from './post-creater/post-creater.service';
+import { PostsComponent } from './posts/posts.component';
+import { PostsService } from './posts/posts.service';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PostsComponent,
-    PostCreaterComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    ReactiveFormsModule,
-    FormsModule
-  ],
-  providers: [PostsService, PostCreaterService],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent, PostsComponent, PostCreaterComponent],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		HttpClientModule,
+		ReactiveFormsModule,
+		FormsModule,
+	],
+	providers: [PostsService, PostCreaterService],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
